d78185d1b5e43be1a95f1c7028d105e91244e583
TIGA <13435648420@139.com>
2020-10-09 11:13:21 +0800

    1、增加证书信任 2、增加kotlin网络请求的逻辑

--- app/src/main/java/com/tiga/coroutines/MainActivity.kt
+++ app/src/main/java/com/tiga/coroutines/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.tiga.coroutines
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initLayout()
+    }
+
+    private fun initLayout() {
+        btnPractice1.setOnClickListener {
+            startActivity(Intent(this@MainActivity,PracticeActivity1::class.java))
+        }
+        btnPractice2.setOnClickListener {
+            startActivity(Intent(this@MainActivity,PracticeActivity2::class.java))
+        }
     }
 }
--- app/src/main/java/com/tiga/coroutines/PracticeActivity2.kt
+++ app/src/main/java/com/tiga/coroutines/PracticeActivity2.kt
@@ -4,15 +4,18 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.tiga.coroutines.entity.Repo
 import com.tiga.coroutines.retrofitApi.GitHubApi
+import com.tiga.coroutines.ssl.TrustAllSSLSocketFactory
 import kotlinx.android.synthetic.main.activity_practice2.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.net.ssl.TrustManagerFactory
 
 class PracticeActivity2 : AppCompatActivity() {
 
@@ -35,27 +38,32 @@ class PracticeActivity2 : AppCompatActivity() {
         }
     }
 
-    private fun initRetrofit(){
+    private fun initRetrofit() {
+        val okHttpClient = OkHttpClient.Builder().sslSocketFactory(
+            TrustAllSSLSocketFactory.newInstance(),
+            TrustAllSSLSocketFactory.TrustAllCertsManager()
+        )
         retrofit = Retrofit.Builder()
             .baseUrl("https://api.github.com/")
+            .client(okHttpClient.build())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
         api = retrofit.create(GitHubApi::class.java)
     }
 
     private fun requestByKt() {
-        if (::retrofit.isInitialized&&::api.isInitialized){
-            GlobalScope.launch(Dispatchers.Main){
-            val repos = api.listReposKt("TonyDash")
-            textView.text = repos[0].name
+        if (::retrofit.isInitialized && ::api.isInitialized) {
+            GlobalScope.launch(Dispatchers.Main) {
+                val repos = api.listReposKt("TonyDash")
+                textView.text ="KT${repos[0].name}"
             }
         }
     }
 
     private fun requestByNormal() {
-        if (::retrofit.isInitialized&&::api.isInitialized){
+        if (::retrofit.isInitialized && ::api.isInitialized) {
             api.listRepos("TonyDash")
-                .enqueue(object :Callback<List<Repo>>{
+                .enqueue(object : Callback<List<Repo>> {
                     override fun onFailure(call: Call<List<Repo>>, t: Throwable) {
                         textView.text = "requestByNormal onFailure"
                     }
